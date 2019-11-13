ab01a08da7655ccfafd27dea96577526129440b1
MegaDedh <ToyotaCami813!@#>
2019-11-14 02:19:20 +0700

    moxysample 2 StateStrategy description

--- moxysample_2_simpleasync/src/main/java/ru/asshands/softwire/moxysample_2_simpleasync/HelloWorldView.kt
+++ moxysample_2_simpleasync/src/main/java/ru/asshands/softwire/moxysample_2_simpleasync/HelloWorldView.kt
@@ -4,10 +4,21 @@ import moxy.MvpView
 import moxy.viewstate.strategy.AddToEndStrategy
 import moxy.viewstate.strategy.StateStrategyType
 
-interface HelloWorldView:MvpView {
+/**
+ * Доступные стратегии:
+ * AddToEndStrategy — выполнить команду и добавить команду в конец очереди
+ * AddToEndSingleStrategy — выполнить команду, добавить ее в конец очереди и удалить все ее предыдущие экземпляры
+ * SingleStateStrategy — выполнить команду, очистить очередь и добавить в нее команду
+ * SkipStrategy — выполнить команду
+ * OneExecuteStrategy — выполнить команду при первой возможности
+ *
+ * Аннотацию стратегий можно разместить перед объявлением интерфейса для всех методов
+ * Так-же можно добавить требуемую аннотацию непосредственно перед методом
+ */
 
-    @StateStrategyType(AddToEndStrategy::class)
+@StateStrategyType(AddToEndStrategy::class)
+interface HelloWorldView : MvpView {
+
     fun showMessage(message: Int)
-    @StateStrategyType(AddToEndStrategy::class)
     fun toggleProgressBar(isLoading: Boolean)
 }
